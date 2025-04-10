0a4a879eec2f45ae4367107032658241bfd916da
Igor Chapy <misha@mate.acedemy>
2025-04-10 16:02:32 +0300

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -3,14 +3,19 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 from taxi.models import Car, Driver
+
+
 class CarForm(forms.ModelForm):
     drivers = forms.ModelMultipleChoiceField(
         queryset=get_user_model().objects.all(),
         widget=forms.CheckboxSelectMultiple,
     )
+
     class Meta:
         model = Car
         fields = "__all__"
+
+
 class DriverCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Driver
@@ -19,20 +24,22 @@ class DriverCreationForm(UserCreationForm):
             "first_name",
             "last_name",
         )
+
     def clean_license_number(self):  # this logic is optional, but possible
         return validate_license_number(self.cleaned_data["license_number"])
+
+
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
         model = Driver
         fields = ["license_number"]
+
     def clean_license_number(self):
         return validate_license_number(self.cleaned_data["license_number"])
 
 
-def validate_license_number(
-    license_number,
-        license_number,
-):  # regex validation is also possible here
+def validate_license_number(license_number):  # Removed the duplicate argument
+    # regex validation is also possible here
     if len(license_number) != 8:
         raise ValidationError("License number should consist of 8 characters")
     elif not license_number[:3].isupper() or not license_number[:3].isalpha():
@@ -55,8 +62,7 @@ class DriverUsernameSearchForm(forms.Form):
     username = forms.CharField(
         max_length=63,
         required=False,
-        widget=forms.TextInput(attrs={
-            "placeholder": "Search Driver by username"}),
+        widget=forms.TextInput(attrs={"placeholder": "Search Driver by username"}),
     )
 
 
@@ -64,6 +70,5 @@ class ManufacturerSearchForm(forms.Form):
     name = forms.CharField(
         max_length=63,
         required=False,
-        widget=forms.TextInput(
-            attrs={"placeholder": "Search Manufacturer by name"}),
+        widget=forms.TextInput(attrs={"placeholder": "Search Manufacturer by name"}),
     )
