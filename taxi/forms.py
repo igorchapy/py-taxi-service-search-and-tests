72b8869f30098a76061d766573bab6a076cc6087
Igor Chapy <misha@mate.acedemy>
2025-04-10 16:00:47 +0300

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -2,21 +2,15 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
-
 from taxi.models import Car, Driver
-
-
 class CarForm(forms.ModelForm):
     drivers = forms.ModelMultipleChoiceField(
         queryset=get_user_model().objects.all(),
         widget=forms.CheckboxSelectMultiple,
     )
-
     class Meta:
         model = Car
         fields = "__all__"
-
-
 class DriverCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Driver
@@ -25,22 +19,19 @@ class DriverCreationForm(UserCreationForm):
             "first_name",
             "last_name",
         )
-
     def clean_license_number(self):  # this logic is optional, but possible
         return validate_license_number(self.cleaned_data["license_number"])
-
-
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
         model = Driver
         fields = ["license_number"]
-
     def clean_license_number(self):
         return validate_license_number(self.cleaned_data["license_number"])
 
 
 def validate_license_number(
     license_number,
+        license_number,
 ):  # regex validation is also possible here
     if len(license_number) != 8:
         raise ValidationError("License number should consist of 8 characters")
@@ -50,3 +41,29 @@ def validate_license_number(
         raise ValidationError("Last 5 characters should be digits")
 
     return license_number
+
+
+class CarModelSearchForm(forms.Form):
+    model = forms.CharField(
+        max_length=63,
+        required=False,
+        widget=forms.TextInput(attrs={"placeholder": "Search Cars by model"}),
+    )
+
+
+class DriverUsernameSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=63,
+        required=False,
+        widget=forms.TextInput(attrs={
+            "placeholder": "Search Driver by username"}),
+    )
+
+
+class ManufacturerSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=63,
+        required=False,
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search Manufacturer by name"}),
+    )
